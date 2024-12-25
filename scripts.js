f7adebb1db4ae4cc39ee8d4ce77558ea19e48561
ghostMan-pac <71212404+ghostMan-pac@users.noreply.github.com>
2024-12-25 22:56:04 +0530

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+
+// Example: Log a message to the console when the page loads
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to ghostMan Tech Blog');
+});
